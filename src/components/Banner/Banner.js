9d89855dcfa2961e0e2c6b093c44754783d169b0
Md Rubel Ahmed Rana <mdrubelahmedrana521@gmail.com>
2022-11-08 01:27:57 +0600

    Banner section completed

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import bannerImg from "../../image/banner.png"
+
+const Banner = () => {
+    return (
+        <div className='bg-gray-900 flex gap-10 justify-between  p-5'>
+            <div>
+                <img className='w-96 h-60' src={bannerImg} alt="" />
+            </div>
+            <div className='text-white w-2/4'>
+                <h3 className='text-4xl font-semibold mb-3'>Welcome to Rubel Digital Studio</h3>
+                <p>Hi dear customer. You are visiting a world class Digital Studio. This is the best studio where you can make video or photo with highest quality. If you are thinking to hire a good photographer or video maker to take your photo or make a reasonable, dymandable video of your any type of events like, Wdding ceromony, Birthday Party, Marriage anniversary ot others you want this studio is the best option for you. so select your event below.</p>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Banner from '../Banner/Banner';
 
 const Home = () => {
     return (
-        <div>
-            
+        <div className='bg-black px-20'>
+            <Banner />
         </div>
     );
 };
